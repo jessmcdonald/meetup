d060cded3156928454be60e416a225675f96b133
jess <51135336+jessmcdonald@users.noreply.github.com>
2019-11-18 15:06:34 +0100

    4.4 desciption pasring

--- src/Event.js
+++ src/Event.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import Button from "react-bootstrap/Button";
-import Card from "react-bootstrap/Card";
+import parse from 'html-react-parser';
 
 class Event extends Component {
   state = {
