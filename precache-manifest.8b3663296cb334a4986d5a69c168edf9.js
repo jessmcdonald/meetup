0f4f40f86812a9e080252d5aca2111182562c815
jess <51135336+jessmcdonald@users.noreply.github.com>
2019-11-18 15:07:30 +0100

    Updates

--- precache-manifest.8b3663296cb334a4986d5a69c168edf9.js
+++ precache-manifest.8b3663296cb334a4986d5a69c168edf9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "432a66c68603dfc574a42b53c6003a9c",
+    "revision": "b0912c8c0c5772244d03f0fc2bbaaf43",
     "url": "/meetup/index.html"
   },
   {
-    "revision": "c68ff01e28cf1196e5a3",
+    "revision": "d3b88468eb38e475a690",
     "url": "/meetup/static/css/2.b0bb7f2c.chunk.css"
   },
   {
-    "revision": "1894f2625c5b719c211b",
+    "revision": "ff1b6b7cc07b0f3a1fae",
     "url": "/meetup/static/css/main.ed090e98.chunk.css"
   },
   {
-    "revision": "c68ff01e28cf1196e5a3",
-    "url": "/meetup/static/js/2.c5164125.chunk.js"
+    "revision": "d3b88468eb38e475a690",
+    "url": "/meetup/static/js/2.d82f8a71.chunk.js"
   },
   {
-    "revision": "1894f2625c5b719c211b",
-    "url": "/meetup/static/js/main.cf9247d3.chunk.js"
+    "revision": "ff1b6b7cc07b0f3a1fae",
+    "url": "/meetup/static/js/main.ad297ebb.chunk.js"
   },
   {
     "revision": "c48a5354a24f34a4db70",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meetup/precache-manifest.d83835d928eb0b1e8db1360b5d151b35.js"
+  "/meetup/precache-manifest.8b3663296cb334a4986d5a69c168edf9.js"
 );
 
 self.addEventListener('message', (event) => {
