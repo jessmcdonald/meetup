47ff342c95d4aa04fd4d000afdba75906383976d
jess <51135336+jessmcdonald@users.noreply.github.com>
2019-11-22 18:46:16 +0100

    4.8 enable service worker & update manifest

--- src/index.js
+++ src/index.js
@@ -11,6 +11,6 @@ ReactDOM.render(<App />, document.getElementById("root"));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+serviceWorker.register();
 
 atatus.config("a75ff1d7a260443bb32c8c4987cb2e3a").install();
